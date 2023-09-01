e48cf281667f12f86a8221ac340a2631474aff7e
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-01 19:23:14 +0530

    tower

--- saves/New World/WriteMCA.py
+++ saves/New World/WriteMCA.py
@@ -3,17 +3,19 @@
 import anvil
 
 region = anvil.EmptyRegion(0, 0)
-side = 15
-tall = 100
+chunk = 1
 
-wall = anvil.Block('minecraft', 'diamond_block')
-floor = anvil.Block('minecraft', 'gold_block')
+side = 16*chunk -1
+tall = 120
 
-# use black_stained_glass as floor to avoid mob spawn
-# floor = anvil.Block('minecraft', 'black_stained_glass')
+wall = anvil.Block('minecraft', 'blue_stained_glass')
+# wall = anvil.Block('minecraft', 'diamond_block')
+
+floor = anvil.Block('minecraft', 'glowstone')
+# floor = anvil.Block('minecraft', 'gold_block')
 
 for x in range(side): # walls
-    for y in range(-63, tall-2):
+    for y in range(-63, tall-1):
         for z in range(side):
             if(x == 0 or x == side - 1 or z == 0 or z == side - 1):
                 region.set_block(wall, x, y, z)
@@ -25,4 +27,4 @@ for x in range(side): # floor
                 region.set_block(floor, x, y, z)
                 
 region.save('region/r.0.0.mca')
-print('Build Complete.')
+print('\nBuild Complete.')
