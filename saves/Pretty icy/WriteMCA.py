f92600247859afaf18ec071c6202344926afbb6f
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-09 22:10:57 +0530

    pixel art

---
+++ saves/Pretty icy/WriteMCA.py
@@ -0,0 +1,26 @@
+
+import anvil
+import pandas as pd
+
+region = anvil.EmptyRegion(0, 0)
+chunk = 1
+
+tall = 120
+radius = 16*chunk -1
+
+df = pd.read_csv('minecraftDot.csv', 
+                   header=None, 
+                   index_col=False)
+
+for x in range(1, len(df.columns)):
+	for z in range(1, len(df.columns)):
+		block = df.iloc[x, z]
+		block = block.split(':')
+
+		print(x-1, z-1, block[1])
+		pixel = anvil.Block('minecraft', block[1])
+		region.set_block(pixel, x-1, 100, z-1)
+
+region.set_block(pixel, x-1, 100, z-1)
+region.save('region/r.0.0.mca')
+print('\nBuild Complete.')
