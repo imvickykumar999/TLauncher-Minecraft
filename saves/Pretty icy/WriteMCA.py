a6671d8704495ea78f6738f2b8e732b188b779ff
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-10 10:32:11 +0530

    missing blocks issue
    https://github.com/kult0922/minecraftDot/issues/3#issuecomment-1712713034

--- saves/Pretty icy/WriteMCA.py
+++ saves/Pretty icy/WriteMCA.py
@@ -14,12 +14,18 @@ df = pd.read_csv('minecraftDot.csv',
 
 for x in range(1, len(df.columns)):
 	for z in range(1, len(df.columns)):
-		block = df.iloc[x, z]
-		block = block.split(':')
+		try:
+			block = df.iloc[x, z]
+			block = block.split(':')
 
-		pixel = anvil.Block('minecraft', block[1])
-		region.set_block(pixel, x-1, 100, z-1)
-		print(x-1, z-1, block[1])
+			pixel = anvil.Block('minecraft', block[1])
+			white_wool = anvil.Block('minecraft', 'white_wool')
+
+			region.set_block(white_wool, x-1, 64, z-1)
+			region.set_block(pixel, x-1, 65, z-1)
+			print(x-1, z-1, block[1])
+		except:
+			pass
 
 region.save('region/r.0.0.mca')
 print('\nBuild Complete.')
